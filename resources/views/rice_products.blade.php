b6455e1ae2899c220b21403012fc7ac7ea8f2c0c
wnafh <w.magayawa.554012@umindanao.edu.ph>
2026-04-14 09:32:43 +0800

    first commit

---
+++ resources/views/rice_products.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <h2>Add Rice Products</h2>
+
+            <form action="POST" action="products/store">
+                @csrf
+
+                <label>Name:</label>
+                <input type="text" name="rice_name" required><br><br>
+                <label>Price per-kilo:</label>
+                <input type="number" name="price_pkilo" required><br><br>
+                <label>Stock Quantit:</label>
+                <input type="text" name="stock_quant" required><br><br>
+                <label>Description:</label>
+                <input type="text" name="rice_desc" required><br><br>
+                
+                <button type="submit">Save</button>
+            </form>
+        
+        
+        </div>
+    </div>
+    
+</body>
+</html>
